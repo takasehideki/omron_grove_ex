30113989cd55faee38b82279ef94870e415c922b
takasehideki <takase@i.kyoto-u.ac.jp>
2019-04-19 15:41:11 +0900

    mix new

---
+++ lib/omron_grove_ex.ex
@@ -0,0 +1,18 @@
+defmodule OmronGroveEx do
+  @moduledoc """
+  Documentation for OmronGroveEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OmronGroveEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/omron_grove_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule OmronGroveExTest do
+  use ExUnit.Case
+  doctest OmronGroveEx
+
+  test "greets the world" do
+    assert OmronGroveEx.hello() == :world
+  end
+end
